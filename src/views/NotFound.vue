742874cce3a6de593e1bb838dfe50c36e4f56ce8
Ting-Yu Huang <iing705037@gmail.com>
2021-11-29 22:36:29 +0800

    [emery] edit: branch test

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,18 +1,14 @@
 // ./src/views/NotFound.vue
 <template>
-    <div>
-        <h1>
-            404 Page Not Found 404
-        </h1>
-        <div class="test">test</div>
-    </div>
+  <div>
+    <h1>404 Page Not Found</h1>
+    <div class="test">test</div>
+  </div>
 </template>
 
 <style scoped lang="sass">
 @import "../styles/notfound.sass"
 
-h1 
+h1
     color: red
-
-
 </style>

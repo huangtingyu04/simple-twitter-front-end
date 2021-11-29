944237e61b3382f8f21a0b06dbc7d529da1cba92
Emery <iing7050737@gmail.com>
2021-11-29 17:44:34 +0800

    [emery] add: test sass

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,18 @@
 // ./src/views/NotFound.vue
 <template>
-  <h1>
-    404 Page Not Found 404
-  </h1>
+    <div>
+        <h1>
+            404 Page Not Found 404
+        </h1>
+        <div class="test">test</div>
+    </div>
 </template>
+
+<style scoped lang="sass">
+@import "../styles/notfound.sass"
+
+h1 
+    color: red
+
+
+</style>

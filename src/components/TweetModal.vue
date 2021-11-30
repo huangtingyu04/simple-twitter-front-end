7a099279d21771496e53c094423bc5ad6ce714e9
mingmoth <mingmoth@gmail.com>
2021-11-30 15:35:04 +0800

    [Guan] add: TweetModal.vue & related sass

--- src/components/TweetModal.vue
+++ src/components/TweetModal.vue
@@ -7,7 +7,7 @@
             class="modal-close"
             data-bs-dismiss="modal"
             aria-label="Close"
-          ></div>
+          >X</div>
         </div>
         <div class="modal-body">
           <div class="modal-body-post">
@@ -22,9 +22,6 @@
               ></textarea>
               <button
                 class="btn-tweet"
-                id="btn-tweet"
-                data-bs-toggle="modal"
-                data-bs-target="#main-post-tweet"
               >
                 推文
               </button>
@@ -41,3 +38,44 @@ export default {
   name: 'TweetModal'
 }
 </script>
+
+<style lang="sass" scoped>
+@import "../styles/_variables"
+.modal
+    border-radius: 14px
+    &-header
+      height: 54px
+    &-body
+      height: 245px
+      padding: 15px
+      &-post
+        display: flex
+        height: 100%
+        &-icon
+          width: 50px
+          height: 50px
+          border-radius: 50%
+          background: $input-label
+          margin-right: 10px
+        form 
+          flex: 1
+          padding-top: 12px
+          position: relative
+          textarea 
+            width: 100%
+            height: 100%
+            border: none
+            resize: none
+            outline: none
+          button
+            position: absolute
+            bottom: 0
+            right: 0
+            width: 66px
+            height: 38px
+            border: none
+            border-radius: 100px
+            background: $mainColor
+            color: $light
+            font-size: 18px
+</style>

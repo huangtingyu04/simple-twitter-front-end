81207c4ccda011e5cc87d6bfb4ee270c6f1d7f22
mingmoth <mingmoth@gmail.com>
2021-11-30 15:04:48 +0800

    [Guan} add: TweetModal,vue & test in About.vue

---
+++ src/components/TweetModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="modal fade" id="tweet-modal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <div
+            class="modal-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></div>
+        </div>
+        <div class="modal-body">
+          <div class="modal-body-post">
+            <div class="modal-body-post-icon"></div>
+            <form action="" class="modal-body-post-body">
+              <textarea
+                name="tweet"
+                id="tweet"
+                cols="30"
+                rows="10"
+                placeholder="有什麼新鮮事?"
+              ></textarea>
+              <button
+                class="btn-tweet"
+                id="btn-tweet"
+                data-bs-toggle="modal"
+                data-bs-target="#main-post-tweet"
+              >
+                推文
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TweetModal'
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tweet-modal">
+      TweetModal
+    </button>
+    <TweetModal />
   </div>
 </template>
+
+<script>
+import TweetModal from '../components/TweetModal.vue'
+export default {
+  name: 'About',
+  components: {
+    TweetModal
+  }
+}
+</script>
 
 

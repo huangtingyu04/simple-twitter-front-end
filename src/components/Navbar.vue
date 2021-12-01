a24fa0ceb4d412722c3d06df3b396d86d4446f33
Ting-Yu Huang <iing705037@gmail.com>
2021-12-01 20:15:36 +0800

    [emery]add: navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,43 +1,52 @@
 <template>
-    <nav class="navbox">
-       <div class="logo">
-           <img src="./../../public/images/Logo.png">
-       </div>
-       <ul>
-           <li>
-               <a href="javascript:;" class="link">
-                   <img class="img" src="./../../public/images/icon_index.png">
-                   <img class="img-hover" src="./../../public/images/icon_index_hover.png">
-                   <span>首頁</span>
-                </a>
-           </li>
-           <li>
-               <a href="javascript:;" class="link">
-                    <img class="img" src="./../../public/images/icon_user.png">
-                    <img class="img-hover" src="./../../public/images/icon_user_hover.png">
-                    <span>個人資料</span>
-               </a>
-           </li>
-           <li>
-               <a href="javascript:;" class="link">
-                    <img class="img" src="./../../public/images/icon_setting.png">
-                    <img class="img-hover" src="./../../public/images/icon_setting_hover.png">
-                   <span>設定</span>
-                </a>
-           </li>
-       </ul>
-       <button type="button" class="tweet-btn">推文</button>
-       <div class="logout">
-           <a href="javascript:;" class="link">
-               <img class="img" src="./../../public/images/icon_logout.png">
-                <img class="img-hover" src="./../../public/images/icon_logout.png">
-               登出
-            </a>
-       </div>
-    </nav>
+  <nav class="navbox">
+    <div class="logo">
+      <img src="../../public/images/Logo.png" />
+    </div>
+    <ul>
+      <li>
+        <a href="javascript:;" class="link">
+          <img class="img" src="../../public/images/icon_index.png" />
+          <img
+            class="img-hover"
+            src="../../public/images/icon_index_hover.png"
+          />
+          <span>首頁</span>
+        </a>
+      </li>
+      <li>
+        <a href="javascript:;" class="link">
+          <img class="img" src="../../public/images/icon_user.png" />
+          <img
+            class="img-hover"
+            src="../../public/images/icon_user_hover.png"
+          />
+          <span>個人資料</span>
+        </a>
+      </li>
+      <li>
+        <a href="javascript:;" class="link">
+          <img class="img" src="../../public/images/icon_setting.png" />
+          <img
+            class="img-hover"
+            src="../../public/images/icon_setting_hover.png"
+          />
+          <span>設定</span>
+        </a>
+      </li>
+    </ul>
+    <button type="button" class="tweet-btn">推文</button>
+    <div class="logout">
+      <a href="javascript:;" class="link">
+        <img class="img" src="../../public/images/icon_logout.png" />
+        <img class="img-hover" src="../../public/images/icon_logout.png" />
+        登出
+      </a>
+    </div>
+  </nav>
 </template>
 
 <style scoped lang="sass">
-    @import '../styles/navbar'
+@import '../styles/navbar'
 </style>
 
--- src/components/PopularUsersCard.vue
+++ src/components/PopularUsersCard.vue
@@ -1,82 +1,94 @@
 <template>
-    <div class="popular-container">
-        <div class="popular-box">
-            <div class="popular-title">Popular</div>
-            <div class="popluar-item">
-                <div class="user-info">
-                    <div class="user-img">
-                        <img src="">
-                    </div>
-                    <div class="user-content">
-                        <div class="name">Pizza Hut</div>
-                        <a class="link" href="javascript:;">@pizzahut</a>
-                    </div>
-                </div>
-                <button type="button" class="follow-btn active">正在跟隨</button>
-            </div>
-            <div class="popluar-item">
-                <div class="user-info">
-                    <div class="user-img">
-                        <img src="">
-                    </div>
-                    <div class="user-content">
-                        <div class="name">McDonald's</div>
-                        <a class="link" href="javascript:;">@McDonalds</a>
-                    </div>
-                </div>
-                <button type="button" class="follow-btn">跟隨</button>
-            </div>
-            <div class="popluar-item">
-                <div class="user-info">
-                    <div class="user-img">
-                        <img src="">
-                    </div>
-                    <div class="user-content">
-                        <div class="name">Pizza Hut</div>
-                        <a class="link" href="javascript:;">@pizzahut</a>
-                    </div>
-                </div>
-                <button class="follow-btn active" type="button">正在跟隨</button>
-            </div>
-            <div class="popluar-item">
-                <div class="user-info">
-                    <div class="user-img">
-                        <img src="">
-                    </div>
-                    <div class="user-content">
-                        <div class="name">McDonald's</div>
-                        <a class="link" href="javascript:;">@McDonalds</a>
-                    </div>
-                </div>
-                <button class="follow-btn" type="button">跟隨</button>
-            </div>
-            <div class="popluar-item">
-                <div class="user-info">
-                    <div class="user-img">
-                        <img src="">
-                    </div>
-                    <div class="user-content">
-                        <div class="name">Pizza Hut</div>
-                        <a class="link" href="javascript:;">@pizzahut</a>
-                    </div>
-                </div>
-                <button class="follow-btn active" type="button">正在跟隨</button>
-            </div>
-            <div class="popluar-item">
-                <div class="user-info">
-                    <div class="user-img">
-                        <img src="">
-                    </div>
-                    <div class="user-content">
-                        <div class="name">McDonald's</div>
-                        <a class="link" href="javascript:;">@McDonalds</a>
-                    </div>
-                </div>
-                <button class="follow-btn" type="button">跟隨</button>
-            </div>
+  <div class="popular-container">
+    <div class="popular-box">
+      <div class="popular-title">Popular</div>
+      <div class="popluar-item">
+        <div class="user-info">
+          <div class="user-img">
+            <img
+              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            />
+          </div>
+          <div class="user-content">
+            <div class="name">Pizza Hut</div>
+            <a class="link" href="javascript:;">@pizzahut</a>
+          </div>
         </div>
+        <button type="button" class="follow-btn active">正在跟隨</button>
+      </div>
+      <div class="popluar-item">
+        <div class="user-info">
+          <div class="user-img">
+            <img
+              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            />
+          </div>
+          <div class="user-content">
+            <div class="name">McDonald's</div>
+            <a class="link" href="javascript:;">@McDonalds</a>
+          </div>
+        </div>
+        <button type="button" class="follow-btn">跟隨</button>
+      </div>
+      <div class="popluar-item">
+        <div class="user-info">
+          <div class="user-img">
+            <img
+              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            />
+          </div>
+          <div class="user-content">
+            <div class="name">Pizza Hut</div>
+            <a class="link" href="javascript:;">@pizzahut</a>
+          </div>
+        </div>
+        <button class="follow-btn active" type="button">正在跟隨</button>
+      </div>
+      <div class="popluar-item">
+        <div class="user-info">
+          <div class="user-img">
+            <img
+              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            />
+          </div>
+          <div class="user-content">
+            <div class="name">McDonald's</div>
+            <a class="link" href="javascript:;">@McDonalds</a>
+          </div>
+        </div>
+        <button class="follow-btn" type="button">跟隨</button>
+      </div>
+      <div class="popluar-item">
+        <div class="user-info">
+          <div class="user-img">
+            <img
+              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            />
+          </div>
+          <div class="user-content">
+            <div class="name">Pizza Hut</div>
+            <a class="link" href="javascript:;">@pizzahut</a>
+          </div>
+        </div>
+        <button class="follow-btn active" type="button">正在跟隨</button>
+      </div>
+      <div class="popluar-item">
+        <div class="user-info">
+          <div class="user-img">
+            <img
+              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            />
+          </div>
+          <div class="user-content">
+            <div class="name">McDonald's</div>
+            <a class="link" href="javascript:;">@McDonalds</a>
+          </div>
+        </div>
+        <button class="follow-btn" type="button">跟隨</button>
+      </div>
     </div>
+  </div>
 </template>
 <style lang="sass" scoped>
-    @import '../styles/popularUsers'
+@import '../styles/popularUsers'
 </style>

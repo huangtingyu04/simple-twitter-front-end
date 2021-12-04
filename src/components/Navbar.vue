017bd81bb12791ea70dc6afe1b335185920a8fea
Ting-Yu Huang <iing705037@gmail.com>
2021-12-04 11:11:10 +0800

    [emery] fix: navbar 破版

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,56 +1,65 @@
 <template>
   <nav class="navbox">
     <div class="logo">
-    <img src="../../public/images/Logo.png" />
+      <img src="../../public/images/Logo.png" />
     </div>
     <ul>
-    <li>
-        <router-link  to="/tweets" class="link">
-        <img class="img" src="../../public/images/icon_index.png" />
-        <img
+      <li>
+        <router-link to="/tweets" class="link">
+          <img class="img" src="../../public/images/icon_index.png" />
+          <img
             class="img-hover"
             src="../../public/images/icon_index_hover.png"
-        />
-        <span>首頁</span>
+          />
+          <span>首頁</span>
         </router-link>
-    </li>
-    <li>
-        <router-link  :to="{name: 'user', params: { id: currentUser.id }}" class="link">
-        <img class="img" src="../../public/images/icon_user.png" />
-        <img
+      </li>
+      <li>
+        <router-link
+          :to="{ name: 'user', params: { id: currentUser.id } }"
+          class="link"
+        >
+          <img class="img" src="../../public/images/icon_user.png" />
+          <img
             class="img-hover"
             src="../../public/images/icon_user_hover.png"
-        />
-        <span>個人資料</span>
+          />
+          <span>個人資料</span>
         </router-link>
-    </li>
-    <li>
-        <router-link class="link" :to="{name: 'user-setting', params: { id: currentUser.id }}">
-        <img class="img" src="../../public/images/icon_setting.png" />
-        <img
+      </li>
+      <li>
+        <router-link
+          class="link"
+          :to="{ name: 'user-setting', params: { id: currentUser.id } }"
+        >
+          <img class="img" src="../../public/images/icon_setting.png" />
+          <img
             class="img-hover"
             src="../../public/images/icon_setting_hover.png"
-        />
-        <span>設定</span>
+          />
+          <span>設定</span>
         </router-link>
-    </li>
+      </li>
     </ul>
     <button
-    type="button"
-    class="tweet-btn"
-    data-bs-toggle="modal"
-    data-bs-target="#tweet-modal"
+      type="button"
+      class="tweet-btn"
+      data-bs-toggle="modal"
+      data-bs-target="#tweet-modal"
     >
-    推文
+      推文
     </button>
     <div class="logout">
-    <router-link  to="/signin" class="link">
+      <router-link to="/signin" class="link">
         <img class="img" src="../../public/images/icon_logout.png" />
-        <img class="img-hover" src="../../public/images/icon_logout_hover.png" />
+        <img
+          class="img-hover"
+          src="../../public/images/icon_logout_hover.png"
+        />
         登出
-    </router-link>
+      </router-link>
     </div>
-    <TweetModal />
+    <TweetModal :current-user="currentUser" />
   </nav>
 </template>
 
@@ -58,16 +67,17 @@
 import TweetModal from "../components/TweetModal.vue";
 
 const dummyUser = {
-    currentUser: {
-        id: 1,
-        name: 'John Doe',
-        account: 'JohnDoe',
-        email: 'John@example.com',
-        password: '12345678',
-        image: 'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-    },
-    isAuthenticated: true
-}
+  currentUser: {
+    id: 1,
+    name: "John Doe",
+    account: "JohnDoe",
+    email: "John@example.com",
+    password: "12345678",
+    image:
+      "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  isAuthenticated: true,
+};
 
 export default {
   name: "Navbar",
@@ -76,29 +86,29 @@ export default {
   },
   data() {
     return {
-       currentUser: {
-            id: -1,
-            name: '',
-            account: '',
-            email: '',
-            password: '',
-            image: ''
-        },
-        isAuthenticated: false
-    }
+      currentUser: {
+        id: -1,
+        name: "",
+        account: "",
+        email: "",
+        password: "",
+        image: "",
+      },
+      isAuthenticated: false,
+    };
   },
   methods: {
     fetchUser() {
-        this.currentUser = {
-            ...this.currentUser,
-            ...dummyUser.currentUser
-        }
-        this.isAuthenticated = dummyUser.isAuthenticated
+      this.currentUser = {
+        ...this.currentUser,
+        ...dummyUser.currentUser,
+      };
+      this.isAuthenticated = dummyUser.isAuthenticated;
     },
   },
   created() {
-    this.fetchUser()
-  }
+    this.fetchUser();
+  },
 };
 </script>
 
--- src/components/TweetModal.vue
+++ src/components/TweetModal.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="modal fade" id="tweet-modal" aria-hidden="true">
-    <div class="modal-dialog" role="document" style="z-index: 9998;">
-      <div class="modal-content" style="z-index: 9999;">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
         <div class="modal-header">
-          <img 
-            src="../../public/images/icon_close.png" alt="" 
+          <img
+            src="../../public/images/icon_close.png"
+            alt=""
             class="modal-close"
             data-bs-dismiss="modal"
-            aria-label="Close">
+            aria-label="Close"
+          />
         </div>
         <div class="modal-body">
           <div class="modal-body-tweet">
             <div class="modal-body-tweet-icon"></div>
-            <form action="" class="modal-body-post-body">
+            <form
+              action=""
+              class="modal-body-post-body"
+              @submit.stop.prevent="createNewTweet(currentUser.id)"
+            >
               <textarea
                 name="tweet"
+                v-model="newTweet"
                 id="tweet"
                 cols="30"
                 rows="10"
@@ -22,6 +29,8 @@
               ></textarea>
               <button
                 class="btn-tweet"
+                :data-bs-dismiss="submitOK"
+                :disabled="newTweet.length > 140"
               >
                 推文
               </button>
@@ -35,10 +44,46 @@
 
 <script>
 export default {
-  name: 'TweetModal'
-}
+  name: "TweetModal",
+  props: {
+    currentUser: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      newTweet: "",
+      checkEmptyInput: false,
+    };
+  },
+  computed: {
+    submitOK() {
+      if (!this.newTweet) {
+        return "";
+      } else {
+        return "modal";
+      }
+    },
+  },
+  methods: {
+    createNewTweet(id) {
+      console.log(id);
+      if (!this.newTweet) {
+        this.checkEmptyInput = true;
+        return;
+      }
+      this.$emit("create-new-tweet", {
+        tweetId: id,
+        text: this.newTweet,
+      });
+      this.newTweet = "";
+      this.checkEmptyInput = false;
+    },
+  },
+};
 </script>
 
 <style lang="sass" scoped>
-  @import '../styles/_modal'
+@import '../styles/_modal'
 </style>

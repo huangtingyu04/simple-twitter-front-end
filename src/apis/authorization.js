dde06fc8a49bea535e6f32d39d8ebecf7adb724f
mingmoth <mingmoth@gmail.com>
2021-12-05 15:36:03 +0800

    [Guan] add utils/apis & apis.author for Authorization

---
+++ src/apis/authorization.js
@@ -0,0 +1,10 @@
+import { apiHelper } from "./../utils/apis"
+
+export default {
+  signIn({ email, password }) {
+    return apiHelper.post('/signin', { email, password })
+  },
+  signUp({ name, account, email, password, checkPassword}) {
+    return apiHelper.post('/users', { name, account, email, password, checkPassword })
+  },
+}

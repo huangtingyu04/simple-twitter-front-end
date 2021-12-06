fd923e5cba2bfd24d8c3a7be130e3786ee19ca3e
mingmoth <mingmoth@gmail.com>
2021-12-06 13:45:59 +0800

    [Guan] add: userUpdate function in UserProfile.vue

--- src/components/UserEditModal.vue
+++ src/components/UserEditModal.vue
@@ -1,93 +1,194 @@
 <template>
   <div class="modal fade" id="user-edit-modal" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <img src="../../public/images/icon_close.png" alt="" class="modal-close"
-            data-bs-dismiss="modal"
-            aria-label="Close">
-          <div class="modal-header-title">編輯個人資料
+      <form action="" @submit="userUpdate">
+        <div class="modal-content">
+          <div class="modal-header">
+            <img
+              src="../../public/images/icon_close.png"
+              alt=""
+              class="modal-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            />
+            <div class="modal-header-title">編輯個人資料</div>
+            <button
+              type="submit"
+              class="btn-save"
+              :disabled="savable"
+              :data-bs-dismiss="submitOK"
+            >
+              儲存
+            </button>
           </div>
-          <button class="btn-save" :disabled="savable">儲存</button>
-        </div>
-        <div class="modal-body">
-          <div class="modal-body-bg">
-            <img src="https://i.imgur.com/ifqzNgs.png" alt="" class="modal-body-bg-photo">
-            <div class="modal-body-bg-option">
-              <img src="../../public/images/icon_uploadPhoto.png" alt="" class="modal-body-bg-option-edit">
-              <img src="../../public/images/icon_delete.png" alt="" class="modal-body-bg-option-delete">
+          <div class="modal-body">
+            <div class="modal-body-bg">
+              <img
+                src="https://i.imgur.com/ifqzNgs.png"
+                alt=""
+                class="modal-body-bg-photo"
+              />
+              <div class="modal-body-bg-option">
+                <img
+                  src="../../public/images/icon_uploadPhoto.png"
+                  alt=""
+                  class="modal-body-bg-option-edit"
+                />
+                <img
+                  src="../../public/images/icon_delete.png"
+                  alt=""
+                  class="modal-body-bg-option-delete"
+                />
+              </div>
+            </div>
+            <div class="modal-body-header">
+              <img
+                src="https://i.imgur.com/RGxqLdu.png"
+                alt=""
+                class="modal-body-header-photo"
+              />
+              <div class="modal-body-header-edit">
+                <img
+                  src="../../public/images/icon_uploadPhoto.png"
+                  alt=""
+                  class="modal-body-header-edit-photo"
+                />
+              </div>
+            </div>
+            <div class="modal-body-form">
+              <form action="">
+                <div class="form-label-group">
+                  <label for="name">名稱</label>
+                  <input
+                    v-model="userName"
+                    type="text"
+                    name="name"
+                    id="name"
+                    :invalid="userNameLength > 50"
+                    required
+                  />
+                  <div class="modal-body-form-limit">
+                    <div
+                      v-show="userNameLength > 50"
+                      class="modal-body-form-limit-alert"
+                    >
+                      字數超出上限!
+                    </div>
+                    <div class="modal-body-form-limit-count">
+                      {{ userNameLength }}<span>/50</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-label-group" id="modal-intro">
+                  <label for="intro">自我介紹</label>
+                  <textarea
+                    v-model="userintro"
+                    name="intro"
+                    id="intro"
+                    cols="30"
+                    rows="10"
+                  ></textarea>
+                  <div class="modal-body-form-limit">
+                    <div
+                      v-show="userIntroLength > 160"
+                      class="modal-body-form-limit-alert"
+                    >
+                      字數超出上限!
+                    </div>
+                    <div class="modal-body-form-limit-count">
+                      {{ userIntroLength }}<span>/160</span>
+                    </div>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
-          <div class="modal-body-header">
-            <img src="https://i.imgur.com/RGxqLdu.png" alt="" class="modal-body-header-photo">
-            <div class="modal-body-header-edit">
-               <img src="../../public/images/icon_uploadPhoto.png" alt="" class="modal-body-header-edit-photo">
-            </div>
-          </div>
-          <div class="modal-body-form">
-            <form action="">
-              <div class="form-label-group">
-                <label for="name">名稱</label>
-                <input v-model="userName" type="text" name="name" id="name"   :invalid="userNameLength > 50" required>
-                <div class="modal-body-form-limit">
-                  <div v-show="userNameLength>50"  class="modal-body-form-limit-alert">字數超出上限!</div>
-                  <div class="modal-body-form-limit-count">{{userNameLength}}<span>/50</span></div>
-                </div>
-              </div>
-              <div class="form-label-group" id="modal-intro">
-                <label for="intro">自我介紹</label>
-                <textarea v-model="userintro" name="intro" id="intro" cols="30" rows="10"></textarea>
-                <div class="modal-body-form-limit">
-                  <div v-show="userIntroLength>160"  class="modal-body-form-limit-alert">字數超出上限!</div>
-                  <div class="modal-body-form-limit-count">{{userIntroLength}}<span>/160</span></div>
-                </div>
-              </div>
-            </form>
-          </div>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'UserSettingModal',
+  name: "UserEditModal",
+  props: {
+    currentUser: {
+      type: Object,
+      required: true,
+    },
+  },
   data() {
     return {
-      userName: 'John Doe',
-      userintro: '',
+      userName: "",
+      userintro: "",
+      userAvatar: "",
+      userCover: "",
       savable: false,
-    }
+    };
   },
   computed: {
     userNameLength() {
-      return this.userName.length
+      return this.userName.length;
     },
     userIntroLength() {
-      return this.userintro.length
-    }
+      return this.userintro.length;
+    },
+    submitOK() {
+      if (!this.savable) {
+        return "modal";
+      } else {
+        return "";
+      }
+    },
   },
   watch: {
     userNameLength() {
-      if(this.userNameLength === 0 || this.userNameLength > 50 || this.userIntroLength > 160) {0
-        return this.savable = true
+      if (
+        this.userNameLength === 0 ||
+        this.userNameLength > 50 ||
+        this.userIntroLength > 160
+      ) {
+        0;
+        return (this.savable = true);
       } else {
-        return this.savable = false
+        return (this.savable = false);
       }
     },
     userIntroLength() {
-      if(this.userNameLength === 0 || this.userNameLength > 50 || this.userIntroLength > 160) {
-        return this.savable = true
+      if (
+        this.userNameLength === 0 ||
+        this.userNameLength > 50 ||
+        this.userIntroLength > 160
+      ) {
+        return (this.savable = true);
       } else {
-        return this.savable = false
+        return (this.savable = false);
       }
-    }
-    
-  }
-}
+    },
+  },
+  created() {
+    this.fetchUser();
+  },
+  methods: {
+    fetchUser() {
+      this.userName = this.currentUser.name;
+      this.userintro = this.currentUser.introduction;
+      this.userAvatar = this.currentUser.userAvatar;
+      this.userCover = this.currentUser.cover;
+    },
+    userUpdate() {
+      this.$emit("user-update", {
+        name: this.userName,
+        introduction: this.userintro,
+        // avatar: this.userAvatar,
+        // cover: this.userCover,
+      })
+    },
+  },
+};
 </script>
 
 <style lang="sass" scoped>
-  @import '../styles/_modal.sass'
+@import '../styles/_modal.sass'
 </style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -7,7 +7,9 @@
           :current-user="currentUser"
           :user="user" />
         <TweetItems :initial-tweets="tweets"/>
-        <UserEditModal :current-user="currentUser" />
+        <UserEditModal 
+          :current-user="currentUser" 
+          @user-update="userUpdate"/>
         <TweetModal :current-user="currentUser" />
       </div>
       <PopularUsersCard />
@@ -23,6 +25,7 @@ const dummyUser = {
     account: "apple",
     email: "apple@example.com",
     password: "12345678",
+    introduction: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do ametsint.",
     image: "https://i.imgur.com/RGxqLdu.png",
   },
 };
@@ -33,7 +36,10 @@ const dummyData = {
     name: "Apple",
     account: "apple",
     email: "apple@example.com",
-    image: "https://i.imgur.com/RGxqLdu.png",
+    password: "12345678",
+    introduction: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do ametsint.",
+    avatar: "https://i.imgur.com/RGxqLdu.png",
+    cover: 'https://i.imgur.com/ifqzNgs.png',
     Followings: [
       {
         id: "1",
@@ -255,11 +261,14 @@ export default {
         id: 0,
         name: '', 
         account: '', 
-        email: '', 
-        image: '', 
+        email: '',
+        introduction: '',
+        avatar: '',
+        cover: '',
         isLiked: false,
         followingsLength: 0, 
-        followersLength: 0
+        followersLength: 0,
+        tweetsCount: 0,
       },
       tweets: [],
     };
@@ -270,12 +279,20 @@ export default {
   methods: {
     fetchData() {
       this.currentUser = dummyUser.currentUser;
-      const { id, name, account, email, image, Tweets, Followings, Followers, isLiked } = dummyData.users;
-      this.user = { id, name, account, email, image,
+      const { id, name, account, email, avatar, cover, introduction, Tweets, Followings, Followers, isLiked } = dummyData.users;
+      this.user = { id, name, account, email, avatar, cover,
         isLiked,
         followingsLength: Followings? Followings.length: 0,
-        followersLength: Followers ? Followers.length: 0 };
+        followersLength: Followers ? Followers.length: 0,
+        introduction,
+        tweetsCount: Tweets ? Tweets.length: 0,
+      };
       this.tweets = Tweets;
+    },
+    userUpdate(payload) {
+      const { name, introduction } = payload
+      this.user.name = name
+      this.user.introduction = introduction
     },
   },
 };

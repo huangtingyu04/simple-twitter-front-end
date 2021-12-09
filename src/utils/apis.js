00804ad71a1590491dc6528456229b5bda86619f
Ting-Yu Huang <iing705037@gmail.com>
2021-12-09 20:52:47 +0800

    [emery]add: http => https

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://twitter-api-2021-teamchris.herokuapp.com/api'
+const baseURL = 'https://twitter-api-2021-teamchris.herokuapp.com/api'
 // const baseURL = 'http://localhost:3000/api'
 
 export const apiHelper = axios.create({ baseURL })

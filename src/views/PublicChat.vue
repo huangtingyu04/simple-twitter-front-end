24695372a48df3d72f99664b9ee835e087273a61
mingmoth <mingmoth@gmail.com>
2021-12-10 16:08:05 +0800

    [Guan] add: Public/Private.vue & chatroom router & Navbar router-link

---
+++ src/views/PublicChat.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="wide-container">Public</div>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  name: 'PublicChat',
+  components: {
+    Navbar,
+  }
+}
+</script>
